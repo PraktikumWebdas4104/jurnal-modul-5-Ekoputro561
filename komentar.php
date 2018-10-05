d121624b6ec7e8dcd1c215a76724faf926855cd2
Eko Putro Abdrrahman Syakir <43515990+Ekoputro561@users.noreply.github.com>
2018-10-05 10:20:50 +0700

    Update komentar.php
    Eko Putro Abdurrahman Syakir
    6701174107
    D3MI4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,22 @@
-<?php
+<form method="POST">
+	<h1>KOMENTAR</h1>
+	<textarea name="komentar"></textarea><br>
+	<input type="submit" name="submit" value="SEND">
+</form>
 
+<?php
+	if (isset($_POST['submit'])) {
+		$komentar=$_POST['komentar'];
+
+		if (str_word_count($komentar)>=5) {
+			echo "<b>Komentar</b> : ";
+			echo $komentar;
+
+			
+		}
+		else{
+			echo "Harus memasukkan lebih dari 5 Kata";
+		}
+		
+	}
 ?>
