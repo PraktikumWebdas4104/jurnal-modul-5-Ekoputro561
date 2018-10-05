46009efce2ceee9d6718cdda4d74ed6b451911ea
Eko Putro Abdrrahman Syakir <43515990+Ekoputro561@users.noreply.github.com>
2018-10-05 10:22:41 +0700

    Update prosesregris.php
    Eko Putro
    6701174107

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,65 @@
-<?php
+<?php 
+	
+	include 'koneksi.php';
+	if (isset($_GET['id'])) {
+		$id		= $_GET['id'];
+		$sql	= "SELECT * FROM akun WHERE id = $id";
 
+		if ($koneksi -> query($sql)) {
+			$result = $koneksi -> query($sql);
+			$row	= $result -> fetch_assoc();
+				
+			}	
+		}
 
+	else {
+		if (isset($_POST['input'])) {
+			include('koneksi.php');
+			
+			if ($koneksi) {
+				$nim  = $_POST['nim'];
+				$nama = $_POST['nama'];
+				$email= $_POST['email'];
+				//$pass = $_POST['pass'];
+				$cekemail= strpos($email, '@gmail.com');
+
+				if (strlen($nim)<=10) {
+					if (is_numeric($nim)== True ) {
+						if (strlen($nama)<= 25) {
+							if (is_string($nama) ==True) {
+								if ($cekemail !== FALSE) {
+									
+								}
+								$sql = $koneksi -> query("
+										INSERT INTO `mhs` (`nim`,`nama`,`email`)
+										VALUES ('$nim','$nama','$email')");
+
+								echo "REGISTRASI SUKSES";
+								
+							}
+							else{
+								echo "Email tidak sesuai";
+							}
+						}
+						else{
+							echo "Panjang Nama tidak boleh lebih dari 25";
+						}
+						
+					}
+					else{
+						echo "NIM Harus angka";
+					}
+					
+				}
+				else{
+					echo "Panjang nim tidak boleh lebih dari 10 digit";
+				}
+				
+			}
+			else{
+				echo "Registrasi Gagal";
+			}
+			
+		}
+	}
 ?>
