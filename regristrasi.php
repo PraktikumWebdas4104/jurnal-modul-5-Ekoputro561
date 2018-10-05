3b5de0725040cb4db28e6ac6d1ee918f49f90250
Eko Putro Abdrrahman Syakir <43515990+Ekoputro561@users.noreply.github.com>
2018-10-05 10:23:39 +0700

    Update regristrasi.php
    Eko Putro
    6701174107

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td></td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td></td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td></td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td></td>
+			<td><input type="Password" name="pass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="input" value="Input"></td>
+		</tr>
 
 
+	</table>
+	
+</form>
 
-</body>
-</html>
+<?php
+	if(isset($_POST['input'])) {
+		include 'prosesregis.php';
+	
+}
+
+  ?>
